84963442e904488cdd6860cf1cd1460ca5e067fe
xz454867105 <454867105@qq.com>
2021-03-28 09:19:44 +0800

    0.1.5

--- load_config.py
+++ load_config.py
@@ -1,8 +1,23 @@
-import json
+import json,os
 
 
 def load():
-    with open('config/general.json') as f:
-        config = json.loads(f.read())
-    speed = config['speed']
-    return speed
+    if os.path.exists('config'):
+        if os.path.exists('config/general.json'):
+            with open('config/general.json','r') as f:
+                config = json.loads(f.read())
+            speed = config['speed']
+            return speed
+        else:
+            with open('config/general.json', 'w') as f:
+                dic = {}
+                dic['speed'] = 1
+                json.dump(dic,f)
+            return 1
+    else:
+        os.mkdir('config')
+        with open('config/general.json', 'w') as f:
+            dic = {}
+            dic['speed'] = 1
+            json.dump(dic, f)
+        return 1
