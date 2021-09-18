7bad15c1f293a150755d9b58573903de1c3e8715
xz454867105 <454867105@qq.com>
2021-09-18 14:07:48 +0800

    1.7.1

--- src/rprint/print.py
+++ src/rprint/print.py
@@ -5,18 +5,14 @@ console = Console()
 
 
 def regulate_print(content):
+    """
+    规范输出的样式
+    :param content:
+    :return:
+    """
     l = len(content)
     if l < 80:
         content = content + (80 - l) * ' '
     else:
         content = content
     console.print(content, end='\r')
-
-
-def normal_print(content):
-    l = len(content)
-    if l < 80:
-        content = content + (80 - l) * ' '
-    else:
-        content = content
-    console.print(content)
