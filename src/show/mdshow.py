4585e0b22678f7573dabd4b8b8928fbee001973c
xz454867105 <454867105@qq.com>
2021-09-18 14:45:00 +0800

    2.0.0

--- src/show/mdshow.py
+++ src/show/mdshow.py
@@ -10,4 +10,4 @@ def print_md():
     console.print(markdown)
     console.input("\n\n请阅读[red]使用须知[/red]，阅读完成后[red]按回车键[/red]继续")
     console.print('\n\n\n\n\n')
-    console.log("[yellow2]开始运行代码[/yellow2]")1
+    console.log("[yellow2]开始运行代码[/yellow2]")
