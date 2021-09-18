cc9c22db489a8a8c52e78dace6b1cf088d99f001
xz454867105 <454867105@qq.com>
2021-09-18 14:44:34 +0800

    2.0.0

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import src.user.getUser as getUser
 import src.course.foreWork as foreWork
 import src.course.doWork as doWork
-
+import src.show.mdshow as mdshow
 
 if __name__ == '__main__':
-    header = {'Accept-Encoding': 'gzip',
-              'Accept-Language': 'zh_CN',
-              'Host': 'i.chaoxing.com',
-              'Connection': 'Keep-Alive',
-              'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 5.1.1; SM-G9350 Build/LMY48Z) '
-                            'com.chaoxing.mobile/ChaoXingStudy_3_5.21_android_phone_206_1 (SM-G9350; Android 5.1.1; '
-                            'zh_CN)_1969814533 '
-              }
+    # 展示md信息文件
+    mdshow.print_md()
+    # 从本地或在线获取用户信息
     usernm, session = getUser.get_session()
+    # 从本地或在线获取课程信息以及各类任务点信息
     jobs, course, mp4, ppt = foreWork.get_forework_done(usernm, session)
+    # 开始完成MP4任务点
     doWork.do_mp4(usernm, course, session, mp4)
+    # 开始完成PPT任务点
     doWork.do_ppt(session, mp4, ppt, usernm, course)
