95f12074a1ab61a14a924b475b0a8e545a8fadd3
Samueli924 <454867105@qq.com>
2024-03-29 15:05:19 +0800

    Fix JSONDecoderError Import

--- api/exceptions.py
+++ api/exceptions.py
@@ -1,12 +1,16 @@
 from loguru import logger
-from requests.exceptions import JSONDecodeError
+# from requests.exceptions import JSONDecodeError
+from json import JSONDecodeError
+
 class BaseException(Exception):
     def __init__(self, _msg: str = None):
         if _msg:
             logger.error(_msg)
 
+
 class LoginError(BaseException):
     pass
+
 
 class FormatError(Exception):
     pass
