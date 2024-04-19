ebc4d692fb7ca13fcbdfb53948e6220583d6e1a6
Tunglies <tunglies@outlook.com>
2024-04-19 08:54:08 +0800

    (#324) 支持多种 api::exceptions::JSONDecodeError 导入方式

--- api/exceptions.py
+++ api/exceptions.py
@@ -1,6 +1,9 @@
 from loguru import logger
-# from requests.exceptions import JSONDecodeError
-from json import JSONDecodeError
+
+try:
+    from requests.exceptions import JSONDecodeError
+except:  # noqa: E722
+    from json import JSONDecodeError
 
 class BaseException(Exception):
     def __init__(self, _msg: str = None):
