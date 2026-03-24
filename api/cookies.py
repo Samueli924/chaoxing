eecf679bf61781e5c49938b1cc785dde4e2caff6
MoYeRanQianZhi <MoYeRanQianZhi@gmail.com>
2026-03-24 21:20:49 +0800

    feat(http): use curl_cffi for chaoxing sessions

--- api/cookies.py
+++ api/cookies.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 import os.path
 
-import requests
-
 from api.config import GlobalConst as gc
 
 
-def save_cookies(session: requests.Session):
+def save_cookies(session):
     buffer=""
     with open(gc.COOKIES_PATH, "w") as f:
         for k, v in session.cookies.items():
